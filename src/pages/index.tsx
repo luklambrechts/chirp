02736287b69fa9c82aa1b064d0a3af5d4df3e45e
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-04-06 21:35:24 +0200

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,10 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -22,7 +23,9 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton></SignInButton>} 
           {!!user.isSignedIn && <SignOutButton></SignOutButton>}
         </div>
-        
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
         
       </main>
     </>
