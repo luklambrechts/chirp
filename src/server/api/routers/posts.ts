58a4837f5da7c587659ec7312f1d19744291b8a9
Luk Lambrechts <luk@Luks-MacBook-Pro.local>
2023-04-13 11:08:35 +0200

    t2:29:44

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -9,12 +8,9 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-
-
 import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
-
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
@@ -24,7 +20,7 @@ const ratelimit = new Ratelimit({
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
    * instance with other applications and want to avoid key collisions. The default prefix is
    * "@upstash/ratelimit"
-   */ 
+   */
   prefix: "@upstash/ratelimit",
 });
 
@@ -63,11 +59,26 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ ctx, input }) =>
+      ctx.prisma.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        take: 100,
+        orderBy: [{ createdAt: "desc" }],
+      })
+    ),
   create: privateProcedure
-    .input(z.object({ content: z.string().emoji("Only emojis are allowed").min(1).max(280) }))
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+      })
+    )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId!;
-      
+
       const { success } = await ratelimit.limit(authorId);
       if (!success) {
         throw new TRPCError({
@@ -75,7 +86,7 @@ export const postsRouter = createTRPCRouter({
         });
       }
       const post = await ctx.prisma.post.create({
-        data: { authorId, content: input.content},
+        data: { authorId, content: input.content },
       });
       return post;
     }),
